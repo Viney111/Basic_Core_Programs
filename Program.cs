0b4dec1cba811050996358375f7117c57148d4ec
Viney <vineykhaneja999@gmail.com>
2022-02-15 12:24:54 +0530

    [Viney] FEATURE ADDED: Win Percenatge of Head Tail

--- Program.cs
+++ Program.cs
@@ -10,7 +10,23 @@ namespace Basic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome tobasic Core Programms");
+            Console.WriteLine("Welcome to basic Core Programms");
+            //CONSTANTS
+            const int MAXNUMBEROFFLIPPINGCOIN = 20;
+            //VARIABLES
+            int noOfFlipping = 0;
+            int tailCounter = 0;
+            int headCounter = 0;
+
+            while (noOfFlipping < MAXNUMBEROFFLIPPINGCOIN)
+            {
+                Random random = new Random();
+                double flipCoin = random.NextDouble();
+                int result = flipCoin < 0.5 ? tailCounter++ : headCounter++;
+                noOfFlipping++;
+            }
+            Console.WriteLine($"The percentage of HEAD winning is {headCounter * 100 / MAXNUMBEROFFLIPPINGCOIN}");
+            Console.WriteLine($"The percentage of TAIL winning is {tailCounter * 100 / MAXNUMBEROFFLIPPINGCOIN}");
         }
     }
 }
