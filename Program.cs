805c204fda200218975eceee133d9dd7c7ee7543
Viney <vineykhaneja999@gmail.com>
2022-02-15 14:00:03 +0530

    [Viney] FEATURE ADDED: Check Odd Even

--- Program.cs
+++ Program.cs
@@ -11,16 +11,11 @@ namespace Basic
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to basic Core Programms");
-            Console.Write("Enter 1st number: ");
-            int number1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter 2nd number: ");
-            int number2 = Convert.ToInt32(Console.ReadLine());
-            int x;
-            x = number1;
-            number1 = number2;  
-            number2 = x;
-            Console.WriteLine($"Numbers after swapping:-\nnumber1:{number1} & number2: {number2}");
-
+            Console.Write("Enter the number: ");
+            //VARIABLES
+            int number = Convert.ToInt32(Console.ReadLine());
+            string result = number % 2 == 0 ? "Number is even" : "Number is odd";
+            Console.WriteLine(result);
         }
     }
 }
